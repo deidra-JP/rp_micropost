ace850ec44eb0ee1723d4cff20fa61fa3301919f
deidra-JP <79629999+deidra-JP@users.noreply.github.com>
2021-05-02 15:33:56 +0900

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,8 +72,8 @@ Rails.application.configure do
     :address => "smtp.gmail.com",
     :port => 587,
     :domain => 'smtp.gmail.com',
-    :user_name => "deidra1971359911@gmail.com",
-    :password => "ereuloslsvxvtkjb",
+    :user_name => "認証メール用の@gmail.com",
+    :password => "上記のパスワード",
     :authentication => 'login'
   }
   end
